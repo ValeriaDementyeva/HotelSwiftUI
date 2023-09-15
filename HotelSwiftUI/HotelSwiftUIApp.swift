d87301502be31a78a54ff8b657757238e7eb142e
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2023-09-15 13:02:10 +0300

    Initial Commit

---
+++ HotelSwiftUI/HotelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelSwiftUIApp.swift
+//  HotelSwiftUI
+//
+//  Created by Валерия Дементьева on 15.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
